2f836d2188683493f973ee1392c61d83154e5b03
Евгений Войтенко <jozik.service@gmail.com>
2021-02-06 00:29:22 +0300

    add styl

--- src/app/model/fileInfo.ts
+++ src/app/model/fileInfo.ts
@@ -1,5 +1,10 @@
+export interface FileInfo {
+  name: string;
+  type: string;
+}
+
 export interface FolderInfo {
   name: string;
   type: string;
-  children: FolderInfo[];
+  children: FolderInfo[] | FileInfo[];
 }
