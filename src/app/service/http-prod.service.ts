06eb7a93cb650f070f1a24a864ddfa65d7b4eede
Евгений Войтенко <jozik.service@gmail.com>
2021-02-08 14:56:48 +0300

    add file data

--- src/app/service/http-prod.service.ts
+++ src/app/service/http-prod.service.ts
@@ -1,11 +1,13 @@
 import { HttpService } from './http.service';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { FilesData } from '../common/file-types';
 
 export class HttpProdService extends HttpService {
   constructor(private httpClient: HttpClient) {
     super();
   }
-  getFiles() {
-    return this.httpClient.get('/assets/data/data.json');
+  getFiles(): Observable<FilesData> {
+    return this.httpClient.get<FilesData>('/assets/data/data.json');
   }
 }
--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -1,7 +1,8 @@
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { FilesData } from '../common/file-types';
 
 @Injectable({ providedIn: 'root' })
 export abstract class HttpService {
-  abstract getFiles(): Observable<any>;
+  abstract getFiles(): Observable<FilesData>;
 }
